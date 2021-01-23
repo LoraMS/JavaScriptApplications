d433f9bd6f3bad647db424209368429f27d350f3
LoraMS <lorashopova@abv.bg>
2021-01-23 19:48:24 +0200

    templates and style changes added

---
+++ scripts/helpers/template.js
@@ -0,0 +1,30 @@
+import Handlebars from 'handlebars';
+import handlebars  from 'handlebars';
+
+class HandlebarsTemplates {
+    constructor(){
+        this.cache = {};
+    }
+
+    getTemplate(name){
+        let cache = this.cache;
+        let _this = this;
+        return new Promise(function(resolve, reject){
+            if(cache[name]){
+                resolve(cache[name]);
+            } else {
+                $.get(`../../templates/${name}.handlebars`, function(templateHtml){
+                    let template = handlebars.compile(templateHtml);
+                    cache[name] = template;
+                    resolve(template);
+                });
+                _this.cache = cache;
+            }
+        });
+        
+    }
+}
+
+let templates = new HandlebarsTemplates();
+
+export { templates as templates };
